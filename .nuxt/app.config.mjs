9c655b94a299252bfc4df1dec543c8e86b02a1d4
henroi joel <sikefouet@gmail.com>
2023-12-01 08:31:56 +0000

    coorection bug réalisations

--- .nuxt/app.config.mjs
+++ .nuxt/app.config.mjs
@@ -1,7 +1,19 @@
 
-import { defuFn } from '/Applications/MAMP/htdocs/bigfive/node_modules/defu/dist/defu.mjs'
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
 
-const inlineConfig = {}
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "73cc223d-d24e-455a-8b19-2fc3c6da96ce"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
 
 
 
